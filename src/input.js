08988fbf5279c6b9634ac0457e9aa854716f1136
vladmoroca <vladmoroca@gmail.com>
2023-10-24 11:22:24 +0300

    more test for input

--- src/input.js
+++ src/input.js
@@ -4,6 +4,13 @@ export const readInput = (filePath) => {
     const data = fs.readFileSync(filePath, "utf8");
     const [generations, columns, rows] = data.match(/\d+/g).map(Number);
     const matrix = data.split("\n").slice(2);
+    let valid = matrix.length == rows
+    console.log(matrix)
+    matrix.map((row) => {
+      if(row.length != columns) valid = false
+    })
+    if(!valid) throw new Error("invalid input")
+
     return {
       rows,
       columns,
--- tests/input.test.js
+++ tests/input.test.js
@@ -5,6 +5,7 @@ jest.mock("fs");
 
 describe("readInput function", () => {
   const pathToFile = "../input.txt";
+
   it("reads and parses valid input file correctly", () => {
     const mockData = "8\n6 6\n......\n...x..\n..xx..\n..xx..\n...x..\n......";
     fs.readFileSync.mockReturnValueOnce(mockData);
@@ -17,4 +18,23 @@ describe("readInput function", () => {
       matrix: ["......", "...x..", "..xx..", "..xx..", "...x..","......"],
     });
   })
+
+  it("throws an error for invalid input(columns)", () => {
+    const mockData = "3 4 2\n....\n....\n";
+    fs.readFileSync.mockReturnValueOnce(mockData);
+
+    expect(() => {
+      readInput("invalidInput.txt");
+    }).toThrow("invalid input");
+  });
+
+  it("throws an error for invalid input(rows)", () => {
+    const mockData = "3 4 2\n......\n......\n";
+    fs.readFileSync.mockReturnValueOnce(mockData);
+
+    expect(() => {
+      readInput("invalidInput.txt");
+    }).toThrow("invalid input");
+  });
+
 });
